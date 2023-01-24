8bb42e61bbfb47f3d39cff8d4f7e3363ddc28737
Peter-Sciberras <sciberrasp@yahoo.com.au>
2023-01-25 01:43:31 +1100

    commit tree 7496dafac0c7649e7766fc467c4a75131fe62519

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => {
+  cats.push(name);
+};
+
+const destructivelyPrependCat = (name) => {
+  cats.unshift(name);
+};
+
+const destructivelyRemoveLastCat = () => {
+  cats.pop();
+};
+
+const destructivelyRemoveFirstCat = () => {
+  cats.shift();
+};
+
+const appendCat = (name) => {
+  const newCats = [...cats];
+  newCats.push(name);
+  return newCats;
+};
+
+const prependCat = (name) => {
+  const newCats = [...cats];
+  newCats.unshift(name);
+  return newCats;
+};
+
+const removeLastCat = () => {
+  const newCats = [...cats];
+  newCats.pop();
+  return newCats;
+};
+
+const removeFirstCat = () => {
+  const newCats = [...cats];
+  newCats.shift();
+  return newCats;
+};
